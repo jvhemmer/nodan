c0876364b0c7b5f889134c8c18f51832b4512c30
jhemmer <hemmer.johann@gmail.com>
2026-04-19 13:32:12 -0400

    Added non-functioning menu bar

--- ui/main_window.py
+++ ui/main_window.py
@@ -3,6 +3,7 @@ from PySide6.QtWidgets import QMainWindow
 from coordinator.coordinator import Coordinator
 from ui.canvas import Canvas
 
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -14,4 +15,15 @@ class MainWindow(QMainWindow):
         self.coordinator = Coordinator(self.canvas)
         self.canvas.coordinator = self.coordinator
 
+        self._build_menu_bar()
         self.setCentralWidget(self.canvas)
+
+    def _build_menu_bar(self) -> None:
+        menu_bar = self.menuBar()
+        file_menu = menu_bar.addMenu("File")
+
+        self.new_action = file_menu.addAction("New...")
+        self.open_action = file_menu.addAction("Open...")
+        file_menu.addSeparator()
+        self.save_action = file_menu.addAction("Save")
+        self.save_as_action = file_menu.addAction("Save as...")
